2e7a58dba1b1afa3d689d423d0bdb00255147bba
David Bushell <david@dbushell.com>
2023-11-23 08:40:50 +0000

    update bumble

--- src/deps.ts
+++ src/deps.ts
@@ -6,5 +6,5 @@ export {existsSync} from 'https://deno.land/std@0.207.0/fs/mod.ts';
 export {deepMerge} from 'https://deno.land/std@0.207.0/collections/mod.ts';
 export {serveFile} from 'https://deno.land/std@0.207.0/http/file_server.ts';
 export * as velocirouter from 'https://deno.land/x/velocirouter@v0.5.0/mod.ts';
-export * as bumble from 'https://raw.githubusercontent.com/dbushell/bumble/v0.8.0/mod.ts';
+export * as bumble from 'https://raw.githubusercontent.com/dbushell/bumble/v0.8.1/mod.ts';
 export * as svelte from 'npm:svelte@4.2.7/compiler';
