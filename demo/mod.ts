7f5a2d4833357f2520b1554690ea7eea1324839a
David Bushell <david@dbushell.com>
2023-12-04 08:30:40 +0000

    demo main branch

--- demo/mod.ts
+++ demo/mod.ts
@@ -1,4 +1,4 @@
-import {DinoServer} from 'https://raw.githubusercontent.com/dbushell/dinossr/v0.10.2/mod.ts';
+import {DinoServer} from 'https://raw.githubusercontent.com/dbushell/dinossr/main/mod.ts';
 
 const dir = new URL('./', import.meta.url).pathname;
 
