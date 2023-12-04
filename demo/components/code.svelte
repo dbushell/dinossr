aed23d13dd44005703ebc786a36f6661e54b5351
David Bushell <david@dbushell.com>
2023-12-04 07:58:39 +0000

    remove demo

--- demo/components/code.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-  export let code;
-  export let language;
-</script>
-
-<pre class="Code"><code class="language-{language}">{@html code}</code></pre>
--- demo/components/docs-alert.svelte
+++
@@ -1,5 +0,0 @@
-<div class="Alert Alert--small">
-  <p>
-    <strong>DinoSsr</strong> is an experimental work in progress and subject to change!
-  </p>
-</div>
