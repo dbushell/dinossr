b2eb36c3b91b63f1d204ce7312633e8860a22af7
David Bushell <david@dbushell.com>
2024-01-16 14:26:56 +0000

    bump deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,10 +1,10 @@
-export * as path from 'https://deno.land/std@0.210.0/path/mod.ts';
-export * as cookie from 'https://deno.land/std@0.210.0/http/cookie.ts';
-export * as base64 from 'https://deno.land/std@0.210.0/encoding/base64.ts';
-export {existsSync} from 'https://deno.land/std@0.210.0/fs/exists.ts';
-export {ensureFileSync} from 'https://deno.land/std@0.210.0/fs/ensure_file.ts';
-export {deepMerge} from 'https://deno.land/std@0.210.0/collections/deep_merge.ts';
-export {serveFile} from 'https://deno.land/std@0.210.0/http/file_server.ts';
-export * as velocirouter from 'https://deno.land/x/velocirouter@v0.5.1/mod.ts';
+export * as path from 'https://deno.land/std@0.212.0/path/mod.ts';
+export * as cookie from 'https://deno.land/std@0.212.0/http/cookie.ts';
+export * as base64 from 'https://deno.land/std@0.212.0/encoding/base64.ts';
+export {existsSync} from 'https://deno.land/std@0.212.0/fs/exists.ts';
+export {ensureFileSync} from 'https://deno.land/std@0.212.0/fs/ensure_file.ts';
+export {deepMerge} from 'https://deno.land/std@0.212.0/collections/deep_merge.ts';
+export {serveFile} from 'https://deno.land/std@0.212.0/http/file_server.ts';
+export * as velocirouter from 'https://deno.land/x/velocirouter@v0.5.2/mod.ts';
 export * as bumble from 'https://raw.githubusercontent.com/dbushell/bumble/v0.12.1/mod.ts';
 export {default as MurmurHash3} from 'https://deno.land/x/murmurhash@v1.0.0/mod.ts';
