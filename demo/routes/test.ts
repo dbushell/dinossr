103922b8e3edca1219ab1d1f966177402d0c9908
David Bushell <david@dbushell.com>
2024-02-01 09:25:58 +0000

    bump deps

--- demo/routes/test.ts
+++ demo/routes/test.ts
@@ -1,4 +1,4 @@
-import {encodeBase64} from 'https://deno.land/std@0.212.0/encoding/base64.ts';
+import {encodeBase64} from 'https://deno.land/std@0.214.0/encoding/base64.ts';
 
 import {default as random} from 'npm:lodash/random.js';
 
