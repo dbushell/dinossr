387ac9e8abde638d9f243566f8b92c22b8ec38e3
David Bushell <david@dbushell.com>
2024-02-23 08:43:31 +0000

    fix dependency version constraint

--- deps.ts
+++ deps.ts
@@ -3,7 +3,7 @@ export * as cookie from 'jsr:@std/http@0.217/cookie';
 export {encodeBase64} from 'jsr:@std/encoding@0.217/base64';
 export {existsSync, ensureFileSync, ensureDirSync} from 'jsr:@std/fs@0.217';
 export {deepMerge} from 'jsr:@std/collections@0.217/deep_merge';
-export {serveDir} from 'jsr:@std/http/file_server';
+export {serveDir} from 'jsr:@std/http@0.217/file_server';
 export * as velocirouter from 'jsr:@ssr/velocirouter@0.7.3';
 export * as svelte from 'npm:svelte@4.2.12/compiler';
 export * as acorn from 'npm:acorn@8.11.3';
