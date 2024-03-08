d19d3ed80027d6971c50f8fb3049ee125ada05d3
David Bushell <david@dbushell.com>
2024-03-08 19:41:48 +0000

    publish action

--- deps.ts
+++ deps.ts
@@ -4,6 +4,6 @@ export {encodeBase64} from 'jsr:@std/encoding@0.219/base64';
 export {existsSync, ensureFileSync, ensureDirSync} from 'jsr:@std/fs@0.219';
 export {deepMerge} from 'jsr:@std/collections@0.219/deep_merge';
 export {serveDir} from 'jsr:@std/http@0.219/file_server';
-export * as velocirouter from 'jsr:@ssr/velocirouter@0.7.5';
+export * as velocirouter from 'jsr:@ssr/velocirouter@0.7.6';
 export * as svelte from 'npm:svelte@4.2.12/compiler';
 export * as acorn from 'npm:acorn@8.11.3';
