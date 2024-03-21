9f9641a5250431025dcac18648bf43b10b950535
David Bushell <david@dbushell.com>
2024-03-21 07:47:53 +0000

    v0.29.0

--- docs/mod.ts
+++ docs/mod.ts
@@ -1,4 +1,4 @@
-import {DinoSsr, type DinoManifest} from 'jsr:@ssr/dinossr@0.28';
+import {DinoSsr, type DinoManifest} from 'jsr:@ssr/dinossr@0.29';
 
 const dir = new URL('./', import.meta.url).pathname;
 
