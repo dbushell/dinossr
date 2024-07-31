cba31e18cf8dd8454920159489b9c159e0b951d4
David Bushell <david@dbushell.com>
2024-07-31 06:17:32 +0100

    update deps with import map

--- deps.ts
+++ deps.ts
@@ -1,16 +1,11 @@
-export * as path from 'jsr:@std/path@0.221';
-export * as cookie from 'jsr:@std/http@0.221/cookie';
-export type {Cookie} from 'jsr:@std/http@0.221/cookie';
-export {encodeBase64} from 'jsr:@std/encoding@0.221/base64';
-export {existsSync, ensureFileSync, ensureDirSync} from 'jsr:@std/fs@0.221';
-export {deepMerge} from 'jsr:@std/collections@0.221/deep-merge';
-export {serveDir} from 'jsr:@std/http@0.221/file-server';
-export {Router as VelociRouter} from 'jsr:@ssr/velocirouter@0.9.0';
-export type {
-  Handle,
-  Method,
-  Platform,
-  Router
-} from 'jsr:@ssr/velocirouter@0.9.0';
-export * as svelte from 'npm:svelte@4.2.12/compiler';
-export * as acorn from 'npm:acorn@8.11.3';
+export * as path from '@std/path';
+export * as cookie from '@std/http/cookie';
+export type {Cookie} from '@std/http/cookie';
+export {encodeBase64} from '@std/encoding';
+export {existsSync, ensureFileSync, ensureDirSync} from '@std/fs';
+export {deepMerge} from '@std/collections/deep-merge';
+export {serveDir} from '@std/http/file-server';
+export {Router as VelociRouter} from '@ssr/velocirouter';
+export type {Handle, Method, Platform, Router} from '@ssr/velocirouter';
+export * as svelte from 'svelte/compiler';
+export * as acorn from 'acorn';
