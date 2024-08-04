791f248d6330a5cc6a675abe7daba0b881fa392d
David Bushell <david@dbushell.com>
2024-08-04 09:25:13 +0100

    fix deploy docs

--- docs/mod.ts
+++ docs/mod.ts
@@ -1,14 +1,7 @@
-import {DinoSsr, type DinoManifest} from 'jsr:@ssr/dinossr@0.31';
+import {DinoSsr} from 'jsr:@ssr/dinossr@0.33';
 
 const dir = new URL('./', import.meta.url).pathname;
 
-let manifest: DinoManifest | undefined;
-if (Deno.env.has('DENO_REGION')) {
-  manifest = await import('file:///src/.dinossr/manifest.js');
-}
-
-const dinossr = new DinoSsr(dir, {
-  manifest
-});
+const dinossr = new DinoSsr(dir);
 
 await dinossr.init();
